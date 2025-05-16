e7dd61f03cccb872d6fe80ffab5981fc45e8d45a
Anton Maltsev <Anton.Maltsev@McKesson.com>
2025-05-16 08:26:48 +0300

    feat: add room and add user to room

--- src/http_server/server.js
+++ src/http_server/server.js
@@ -6,17 +6,17 @@ const wss = new WebSocketServer({port: PORT});
 console.log(`WebSocket server started on port ${PORT}`);
 
 const players = [];
+const rooms = [];
 
 wss.on('connection', (ws) => {
     console.log('New player connected');
 
     ws.on('message', (message) => {
-        // console.log(`Accepted message: ${message}`);
-        // ws.send(`Response from server: ${message}`);
 
         const data = JSON.parse(message);
         console.log(`Received command: ${data.type}`);
 
+        // REGISTRATION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         if (data.type === "reg") {
             const {name, password} = data.data;
             let player = players.find(p => p.name === name);
@@ -30,7 +30,7 @@ wss.on('connection', (ws) => {
                     data: {name, index: player.index, error: false, errorText: ""},
                     id: 0
                 }));
-                console.log(`Игрок ${name} зарегистрирован`);
+                console.log(`Player ${name} registered`);
             } else if (player.password === password) {
                 // succeed
                 ws.send(JSON.stringify({
@@ -49,6 +49,62 @@ wss.on('connection', (ws) => {
                 console.log(`Auth error: invalid password for ${name}`);
             }
         }
+
+        // ROOMS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (data.type === "create_room") {
+            const indexRoom = rooms.length + 1;
+            const newRoom = {roomId: indexRoom, players: []};
+            rooms.push(newRoom);
+
+            ws.send(JSON.stringify({
+                type: "update_room",
+                data: rooms.filter(r => r.players.length === 1),
+                id: 0
+            }));
+
+            console.log(`Room is created with ID: ${indexRoom}`);
+        }
+
+        if (data.type === "add_user_to_room") {
+            const {indexRoom} = data.data;
+            const room = rooms.find(r => r.roomId === indexRoom);
+
+            if (!room) {
+                ws.send(JSON.stringify({
+                    type: "add_user_to_room",
+                    data: {indexRoom, status: "error", message: "Room does not exist"},
+                    id: 0
+                }));
+                console.log(`Error: Room ${indexRoom} does not exist.`);
+            } else if (room.players.length >= 2) {
+                ws.send(JSON.stringify({
+                    type: "add_user_to_room",
+                    data: {indexRoom, status: "error", message: "Room is full"},
+                    id: 0
+                }));
+                console.log(`Error: Room ${indexRoom} is full`);
+            } else {
+                // room.players.push({ws});
+                room.players.push({ name: data.name, ws });
+
+                ws.send(JSON.stringify({
+                    type: "add_user_to_room",
+                    data: {indexRoom, status: "success", message: "You joined the room"},
+                    id: 0
+                }));
+                console.log(`Player joined the room ${indexRoom}`);
+
+                if (room.players.length === 2) {
+                    room.players.forEach(p => p.ws.send(JSON.stringify({
+                        type: "create_game",
+                        data: {idGame: indexRoom},
+                        id: 0
+                    })));
+                    console.log(`Game is started in room ${indexRoom}`);
+                }
+            }
+        }
+
 
     });
 
